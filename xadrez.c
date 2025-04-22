38ddb865e25ed6c703f7c72b11daeb67c88c7a17
Jimmysaippa <suporteinformaticanf@gmail.com>
2025-04-22 00:51:20 +0000

    nivel aventureiro xadrez

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,8 @@
    int main()  {
 
     int opcao, i, casa; // variaveis
+    int movimentocompleto;
+    char letra;
 
 
     printf("\n *** Jogo de Xadrez para Iniciante *** \n");
@@ -19,7 +21,8 @@
     printf("1. Mova a Torre\n");
     printf("2. Mova o Bispo\n");
     printf("3. Mova a Rainha\n");
-    printf("4. Sair\n");
+    printf("4. Mova com o Cavalo\n");
+    printf("5. Sair\n");
     printf("\n");
     printf("Escolha uma opção: \n");
     scanf("%d", &opcao);
@@ -55,7 +58,7 @@
 
     case 3:
         printf("Você escolheu a rainha\n");
-        printf("Digite a quantidad de casas quer andar:");
+        printf("Digite a quantidade de casas dejesa andar:");
         scanf("%d", &casa);
 
         for ( i = 0; i < casa; i++)  // icremento variavel e confição repetição for
@@ -66,7 +69,26 @@
         
         break;
 
+        
     case 4:
+        printf("Você escolher o Cavalo \n");
+        printf("Digite a quantidade de casa deseja andar:");
+        scanf("%d", &casa);
+             
+            for (movimentocompleto = 1; movimentocompleto <= 1; movimentocompleto++)
+            {
+                for ( i = 0; i < casa; i++)
+                {
+                    printf("Cima \n");
+                        
+                }
+                printf("Direita\n");          
+            }
+
+           
+        break;
+
+    case 5:
         printf("Saindo do Jogo ....\n");
         break;
     
@@ -75,7 +97,7 @@
         break;
     }
 
-} while ( opcao != 4); // condição de repedicao do while
+} while ( opcao != 5); // condição de repedicao do while
 
 return 0;
 
